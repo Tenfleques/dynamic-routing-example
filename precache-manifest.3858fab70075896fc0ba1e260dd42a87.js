c36d044413b41e559fc852601db3354ba70389c0
Tendai Chikake <tendaichikae@gmail.com>
2019-05-19 14:33:42 +0300

    Updates

---
+++ precache-manifest.3858fab70075896fc0ba1e260dd42a87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036dff31d47eacb524552a7e2375d88b",
+    "url": "/dynamic-routing-example/index.html"
+  },
+  {
+    "revision": "c74bf8b3feefa04ad68b",
+    "url": "/dynamic-routing-example/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "002394b6a5e6ff327509",
+    "url": "/dynamic-routing-example/static/js/2.1714a347.chunk.js"
+  },
+  {
+    "revision": "c74bf8b3feefa04ad68b",
+    "url": "/dynamic-routing-example/static/js/main.69e87caa.chunk.js"
+  },
+  {
+    "revision": "4e98170a1c1199a69d56",
+    "url": "/dynamic-routing-example/static/js/runtime~main.40790f82.js"
+  }
+]);
